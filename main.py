639be2ebf1d056da3c5955b5f00e08534fe0e747
ArtemZhuravlev <Utah954@gmail.com>
2023-09-22 16:47:55 +0300

    Add working code for bot to "main.py"

--- main.py
+++ main.py
@@ -8,42 +8,47 @@ def parse(user_input):
     args = user_input_list[1:]
     return (command, args)
 
+#Опис функції, що обробляє винятки
 def input_error(function):
     def inner(*args):
         try:
             return function(*args)
         except KeyError:
-            return "The name is not in contacts. Enter user name, please."
+            return "The name isn't in contacts. Enter your user name, please."
         except ValueError:
-            return "ValueError: Give me name and phone, please."
+            return "ValueError: Give me your name and phone, please."
         except IndexError:
-            return "IndexError: Give me name and phone, please."
+            return "IndexError: Give me your name and phone, please."
         except TypeError:
             return "You entered invalid numbers of arguments for this command."
     return inner
 
+#Опис функції,яка зберігає у пам'яті (у словнику наприклад) новий контакт
 @input_error
 def add_contact(name, phone):
     contacts[name] = phone
     return f"Contact {name}: {phone} was successfully added."
 
+#Опис функції, яка зберігає в пам'яті новий номер телефону існуючого контакту
 @input_error
 def change_contact(name, phone):
     contacts.update({name: phone})
     return f"Contact {name}: {phone} was successfully changed."
 
+#Опис функції, яка виводить у консоль номер телефону для зазначеного контакту
 @input_error
 def get_phone(name):
     phone = contacts[name]
     return f"For {name} the phone is {phone}."
 
+#Опис функції, яка виводить всі збереженні контакти з номерами телефонів у консоль
 def show_all():
     phone_book = ''
     for name, contact in contacts.items():
         phone_book += f"{name} : {contact}\n"
     return phone_book
 
-def greeting():
+def sayhello():
     return "How can I help you?"
 
 def end():
@@ -51,7 +56,7 @@ def end():
 
 def main():
 
-    handler_commands = {"hello": greeting,
+    handler_commands = {"hello": sayhello,
                         "add": add_contact,
                         "change": change_contact,
                         "phone": get_phone,
